13d8321723afc4a343bce8935be180f15e99694b
BoShurik <boshurik@gmail.com>
2017-03-18 21:25:13 +0300

    Allow help command for every unmatched request

---
+++ src/AppBundle/Telegram/HelpCommand.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: boshurik
+ * Date: 18.03.17
+ * Time: 21:21
+ */
+
+namespace AppBundle\Telegram;
+
+use BoShurik\TelegramBotBundle\Telegram\Command\HelpCommand as BaseCommand;
+use TelegramBot\Api\Types\Message;
+
+class HelpCommand extends BaseCommand
+{
+    /**
+     * @inheritDoc
+     */
+    public function isApplicable(Message $message)
+    {
+        return true;
+    }
+}
