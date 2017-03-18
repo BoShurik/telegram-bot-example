d4e93837a0401800105d113ce9af2a7679fe25c2
BoShurik <boshurik@gmail.com>
2017-03-18 21:15:31 +0300

    Hello World command

---
+++ src/AppBundle/Telegram/HelloCommand.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * User: boshurik
+ * Date: 18.03.17
+ * Time: 21:10
+ */
+
+namespace AppBundle\Telegram;
+
+use BoShurik\TelegramBotBundle\Telegram\Command\AbstractCommand;
+use BoShurik\TelegramBotBundle\Telegram\Command\PublicCommandInterface;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Types\Message;
+
+class HelloCommand extends AbstractCommand implements PublicCommandInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return '/hello';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDescription()
+    {
+        return 'Example command';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(BotApi $api, Message $message)
+    {
+        preg_match(self::REGEXP, $message->getText(), $matches);
+        $who = !empty($matches[3]) ? $matches[3] : "World";
+
+        $text = sprintf('Hello *%s*', $who);
+        $api->sendMessage($message->getChat()->getId(), $text, 'markdown');
+    }
+}
